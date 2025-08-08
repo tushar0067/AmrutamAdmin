783b96d8df0ba9e59da8aaa9eb7cc9d8617c3ba2
tushar0067 <tushar.2226csit1003@kiet.edu>
2025-08-08 19:42:56 +0530

    final commit

--- Amrutam-Frontend/src/components/App.js
+++ Amrutam-Frontend/src/components/App.js
@@ -10,7 +10,7 @@ import { LoginPage } from './LoginPage';
 
 // The API base URL
 
-const API_URL = 'http://localhost:5000/api/ingredients';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/ingredients';
 
 function App() {
   // Initialize auth state by checking localStorage for a token
